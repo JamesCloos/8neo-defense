a8aa163c68f019fb33432b9045dfbea32495a5f2
JamesCloos <42454485+JamesCloos@users.noreply.github.com>
2019-05-21 20:11:30 +0800

    Update Base.java

--- src/main/java/raidzero/frc/robot/Base.java
+++ src/main/java/raidzero/frc/robot/Base.java
@@ -1,3 +1,4 @@
+    
 package raidzero.frc.robot;
 
 import com.revrobotics.CANSparkMax;
@@ -18,17 +19,29 @@ public class Base {
             LDrive[3] = new CANSparkMax(5 , MotorType.kBrushless);
             RDrive[2] = new CANSparkMax(6, MotorType.kBrushless);
             RDrive[3] = new CANSparkMax(7, MotorType.kBrushless);
-        for(int i = 1; i < 4; i++) {
+        for(int i = 0; i < 4; i++) {
+            LDrive[i].setOpenLoopRampRate(1);
+            RDrive[i].setOpenLoopRampRate(1);
+            LDrive[i].setSmartCurrentLimit(40, 20);
+            RDrive[i].setSmartCurrentLimit(40, 20);
+            LDrive[i].setSecondaryCurrentLimit(45);
+            RDrive[i].setSecondaryCurrentLimit(45);
             LDrive[i].setIdleMode(IdleMode.kBrake);
             RDrive[i].setIdleMode(IdleMode.kBrake);
+            LDrive[i].burnFlash();
+            RDrive[i].burnFlash();
+        }
+        for(int i = 1; i < 4; i++) {
             LDrive[i].follow(LDrive[0],false);
             RDrive[i].follow(RDrive[0],false);
         }
     }
     
     public static void run(double lPower, double rPower) {
-            LDrive[0].set((lPower/Math.abs(lPower))*Math.sqrt(Math.abs(lPower)*0.5));
-            RDrive[0].set(-(rPower/Math.abs(rPower))*Math.sqrt(Math.abs(rPower))*0.5);
+            LDrive[0].set(lPower*lPower);
+            RDrive[0].set(rPower*rPower);
+            //LDrive[0].set((lPower/Math.abs(lPower))*Math.sqrt(Math.abs(lPower)*0.5));
+            //RDrive[0].set(-(rPower/Math.abs(rPower))*Math.sqrt(Math.abs(rPower))*0.5);
             //LDrive[0].set(0);
             //RDrive[0].set(0);
     }
